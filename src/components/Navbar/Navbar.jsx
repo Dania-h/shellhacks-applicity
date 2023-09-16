906773e761b3d81690ceb9c6768505fcab58049e
Christian Ocampo <cocampo1005@yahoo.com>
2023-09-16 10:59:52 -0400

    Finished navbar look

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import "../Navbar/Navbar.scss";
 import logo from "../../assets/logo.png";
+import arrowUpright from "../../assets/icons/arrow-upright.png";
 
 function Navbar() {
     return (
         <nav className="navbar">
             <img className="navbar__logo" src={logo} alt="logo" />
             <input className="navbar__search" type="text" placeholder="Search Companies..." />
-            <p>Login</p>
+            <a className="navbar__link" href="/#">
+                <p className="navbar__link-text">Login</p>
+                <img className="navbar__link-icon" src={arrowUpright} alt="login arrow" />
+            </a>
         </nav>
     )
 }
