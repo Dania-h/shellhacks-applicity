13f82b48724abdffca99adf3363687fa1ae23ac6
Dania <dabeaher@yahoo.com>
2023-09-16 20:24:08 -0400

    Finished styling dashboard nav and began tracking table

---
+++ src/components/TrackerTable/TrackerTable.jsx
@@ -0,0 +1,11 @@
+import './TrackerTable.scss';
+
+function TrackerTable() {
+    return (
+        <section className='tracker-table'>
+            <h1>Job Tracker</h1>
+        </section>
+    );
+  }
+  
+  export default TrackerTable;
